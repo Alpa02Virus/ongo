282c469a04fffa6621209c665d0f1cf77403a039
Ulziitogtokh <122375197+Alpa02Virus@users.noreply.github.com>
2023-09-10 12:09:50 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var body = document.getElementsByTagName('body')[0];
+console.log(body);
+var colors = ['brown','aqua','green'];
+var i =-1;
+function color(){
+    i++;
+    body.style.backgroundColor=color[i];
+
+}   
